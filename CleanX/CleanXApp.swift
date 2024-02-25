9c3279a3e8dc1cf4a19613722f1e8e843697e92f
Nikita Arshinov <narshinov01@gmail.com>
2024-02-25 20:21:46 +0300

    Initial commits

---
+++ CleanX/CleanXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanXApp.swift
+//  CleanX
+//
+//  Created by Nikita Arshinov on 25.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CleanXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
