8fb93477a0e9f1bfd05e246fdec62e6c5a375797
Nikita Arshinov <narshinov01@gmail.com>
2024-02-27 15:19:28 +0300

    Setup base preferences

---
+++ CleanX/App/Duplicates/PhotoVideo/PhotoVideoView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoVideoView.swift
+//  CleanX
+//
+//  Created by Nikita Arshinov on 27.02.24.
+//
+
+import SwiftUI
+
+struct PhotoVideoView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    PhotoVideoView()
+}
